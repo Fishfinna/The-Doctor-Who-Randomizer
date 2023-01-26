3ab9ca2aca4660eb3327e9a98d9ce91840d0061c
Shanti <fishfinna12@gmail.com>
2023-01-25 23:06:13 -0800

    worked on ui and set up new data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*{html,js}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      DoctorWho: ["logo-font.ttf"],
+    extend: {
+      fontFamily: {
+        neue: ['"Bebas Neue"'],
+      },
     },
   },
-  plugins: [require("@tailwindcss/typeography")],
+  plugins: [require("daisyui")],
 };
